f9d306adbbc64899dfc733423e12e5bdb6bec3ac
hummingbird20 <42376727+hummingbird20@users.noreply.github.com>
2018-10-31 23:44:18 +0530

    Add files via upload

---
+++ cancel_tickets.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+     <title>cancel tickets</title>
+	 <!-- Meta tag Keywords -->
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Shine Login Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements"
+	/>
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Meta tag Keywords -->
+	<!-- css files -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //css files -->
+	<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i" rel="stylesheet">
+	<!-- //online-fonts -->
+</head>
+    
+   
+  <body>
+  
+    <!--header-->
+	<div class="header-w3l">
+		<center><h1>Cancel ticket</center></h1>
+	</div>
+	<!--//header-->
+	<div class="main-content-agile">
+		<div class="sub-main-w3">
+			
+			<form action="#" method="post">
+				<div class="pom-agile">
+					<span class="fa fa-key" aria-hidden="true"></span>
+					<label for="PNR">PNR</label>
+					<input type="text" name="PNR" value=""><br>
+				</div>
+				<div class="pom-agile">
+					<span class="fa fa-key" aria-hidden="true"></span>
+					 <label for="ticket_id">Ticket ID</label>
+					<input type="text" name="ticket_id" value=""><br>
+					</div>
+					<a href="#">cancel ticket</a>
+					<div class="clear"></div>
+				</div>
+				</div>
+					<a href="#">logout</a>
+					<div class="clear"></div>
+					<a href="user_homepage.php">go back</a><br>
+					<div class="clear"></div>
+				
+			
+		
+	</form>
+	</div>
+	</div>
+	
+	<!--//main-->
+	
+      </body>
+</html>
